c55f5bf6ab887e50512ec6ccbe6b5a4d26a8a4ea
MintHayv1i <norchdew@mail.ru>
2025-09-26 13:29:36 +0400

    Update lab3_A_9.py

--- lab3_A_9.py
+++ lab3_A_9.py
@@ -1,14 +1,14 @@
-#Лабораторная работа номер 3 (task A 9)
+ #Лабораторная работа номер 3 (task A 9)
 
-#Задача 1
+ #Задача 1
 em = input('Введите адрес: ')
 emsp = em.split('@')
 print(em)
 print(emsp)
 if len(em) >= 5 and '.' in emsp[-1]:
-        print('Да')
+         print('Да')
 else:
-    print('Нет')
+     print('Нет')
 
 #Задача 2
 a = input()
@@ -19,3 +19,8 @@ if ',' in a:
 print(a)
 m = (list(map(int, a)))
 print(sum(m))
+sumM = (sum(m))
+lenM = len(m)
+print(lenM)
+af = sumM/lenM
+print(af)
