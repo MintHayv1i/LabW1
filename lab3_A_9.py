76aea7a33106d262ca47fa533b5d4fdb9bc9d0ce
MintHayv1i <norchdew@mail.ru>
2025-09-26 13:07:20 +0400

    Add files via upload

---
+++ lab3_A_9.py
@@ -0,0 +1,21 @@
+#Лабораторная работа номер 3 (task A 9)
+
+#Задача 1
+em = input('Введите адрес: ')
+emsp = em.split('@')
+print(em)
+print(emsp)
+if len(em) >= 5 and '.' in emsp[-1]:
+        print('Да')
+else:
+    print('Нет')
+
+#Задача 2
+a = input()
+if ',' in a:
+    a = a.replace(',',' ')
+    a = a.split()
+
+print(a)
+m = (list(map(int, a)))
+print(sum(m))
